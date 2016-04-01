78f1c2ef1662e7b02e59de2729bf2a2ed1727796
brambleshadow4 <brambleshadow4@gmail.com>
2016-04-01 12:17:04 -0400

    MLP Cards added to site!
    New project added

---
+++ MLP Cards/Cards.js
@@ -0,0 +1,421 @@
+var CardDatabase = 
+[
+	{
+		name:"Twilight Sparkle",
+		image: "ts1.png",
+		info: "Twilight Sparkle is the prized pupil of Princess Celestia. Although she is from Canterlot, she now resides in Ponyville where she studies the magic of friendship.",
+		categories: ["Mane 6","Unicorn","Character"]
+	},
+	{
+		name: "Spike", 
+		image: "spike.png", 
+		info: "Spike is Twilight Sparkle's assistant. He's always there to help Twilight out with whatever she needs.",
+		categories: ["Non-pony","Character"]
+	},
+	{
+		name: "Applejack", 
+		image: "aj1.png", 
+		info: "Applejack is the head of the Apple family. She lives and works at Sweet Apple Acres harvesting, you guessed it, apples.",
+		categories: ["Mane 6", "Earth Pony","Character","Apple Family"]
+	},
+	{
+		name: "Rainbow Dash", 
+		image: "rd1.png", 
+		info: "Rainbow Dash is the fastest pegasus in Ponyville! She spends her days practicing tricks in the sky, and she hopes to join the Wonderbolts someday.",
+		categories: ["Mane 6","Pegasus","Character"]
+	},
+	{
+		name: "Rarity", 
+		image: "rr1.png", 
+		info: "Rarity is a fashionista who lives in Ponyville. She's always making new dresses which showcase her masterful talent.",
+		categories: ["Mane 6","Unicorn","Character"]
+	},
+	{
+		name: "Fluttershy", 
+		image: "fs1.png", 
+		info: "Fluttershy is super sweet, though she's also very shy. She lives outside Ponyville in her cottage, and she takes care of the animals there.",
+		categories: ["Mane 6","Pegasus","Character"]
+	},
+	{
+		name: "Pinkie Pie", 
+		image: "pp1.png", 
+		info: "Pinkie Pie is one heck of a party lover, as she loves talking to everypony! She lives and works at Sugarcube Corner in Ponyville.",
+		categories: ["Mane 6","Earth Pony","Character"]
+	},
+	{
+		name: "Apple Bloom",
+		image: "ab1.png",
+		info: "Apple Bloom is Applejack's younger sister. She's friends with Scootaloo and Sweetie Belle, who collectively form the Cutie Mark Crusaders.",
+		categories: ["CMC","Earth Pony","Character","Apple Family"]
+	},
+	{
+		name: "Sweetie Belle",
+		image: "sb1.png",
+		info: "Sweetie Bell is Rarity's younger sister. She's friends with Apple Bloom and Scootaloo, who collectively form the Cutie Mark Crusaders.",
+		categories: ["CMC","Unicorn","Character"]
+	},
+	{
+		name: "Scootaloo",
+		image: "sl1.png",
+		info: "Scootaloo doesn't have any known family, but she frequently looks up to Rainbow Dash as a sister. She along with Sweetie Belle and Applebloom make up the Cutie Mark Crusaders.",
+		categories: ["CMC","Pegasus","Character"]
+	},
+	{
+		name: "Princess Celestia",
+		image: "celestia.png",
+		info: "Princess Celestia is the kind, benevolent ruler of all of Equestria. She is also responsible for raising the sun each and every day.",
+		categories: ["Alicorn","Character"]
+	},
+	{
+		name: "Princess Luna",
+		image: "luna.png",
+		info: "Princess Luna is Princess Celestia's younger sister. She raises the moon at dusk to begin the night.",
+		categories: ["Alicorn","Character"]
+	},
+	{
+		name: "Princess Cadance",
+		image: "cadance.png",
+		info: "Princess Cadence is the princess of love. She rules over the Crystal Empire in the north.",
+		categories: ["Alicorn","Character"]
+	},
+	{
+		name: "Princess Twilight",
+		image: "pts.png",
+		info: "Just like normal Twilight Sparkle, except she's now a Princess (from the end of S3 onward)",
+		categories: ["Alicorn","Character","Mane 6"]
+	},
+	{
+		name: "Zecora",
+		image: "zecora.png",
+		info: "Zecora is a zebra from a faraway land. She lives in the Everfree forest where she brews potions of all kinds.",
+		categories: ["Non-pony", "Character"]
+	},
+	{
+		name: "Big McIntosh",
+		image: "bmac.png",
+		info: "Big McIntosh is Applejack's older brother. He is a hard working stallion who hardly speaks other than to say \"Eeyup\" or \"Nope.\"",
+		categories: ["Apple Family", "Earth Pony", "Character"]
+	},
+	{
+		name: "Granny Smith",
+		image: "gsmith.png",
+		info: "Granny Smith is the oldest member of the Apple family. Although she doesn't help out on the farm much these days, she was there when Sweet Apple Acres was founded.",
+		categories: ["Apple Family", "Earth Pony", "Character"]
+	},
+	{
+		name: "Mayor Mare",
+		image: "mayor.png",
+		info: "Mayor Mare is the mayor of Ponyville. She presides over ceremonies and special events within the town.",
+		categories: ["Earth Pony", "Character"]
+	},
+	{
+		name: "Diamond Tiara",
+		image: "diamond.png",
+		info: "Diamond Tiara is the bully from the series. She often makes fun of Apple Bloom, Scootaloo, and Swetie Belle for being 'Blank Flanks.'",
+		categories: ["Earth Pony", "Character", "Antagonist"]
+	},
+	{
+		name: "Silver Spoon",
+		image: "spoon.png",
+		info: "Silver Spoon is Diamond Tiara's partner in crime. While she definitely joins in on the bullying, she rarely starts it herself.",
+		categories: ["Earth Pony", "Character"]
+	},
+	{
+		name: "Shining Armor",
+		image: "sarmor.png",
+		info: "Shining Armor is Twilight Sparkle's older brother and Princess Cadance's wife. He is the captain of the royal guard, and he presides in Canterlot.",
+		categories: ["Unicorn", "Character"]
+	},
+	{
+		name: "Sunset Shimmer",
+		image: "sunset.png",
+		info: "Sunset Shimmer is a former apprentice of Princess Celestia. However, when her studies did not take her where she wanted to be quickly enough, she pursued a more ambitions path.",
+		categories: ["Unicorn", "Character", "Antagonist"]
+	},
+	{
+		name: "Starlight Glimmer",
+		image: "starlight.png",
+		info: "Starlight Glimmer is a unicorn with quite a prowess for magic. She spreads an ideology of equality, as she would prefer to live in a society where no pony has individual talents or cutie marks. ",
+		categories: ["Unicorn", "Character", "Antagonist"]
+	},
+	{
+		name: "Discord",
+		image: "discord.png",
+		info: "Discord is the spirit of disharmony. He wields very powerful magic which he uses to inflict all kinds of chaos and disorder.",
+		categories: ["Character","Non-pony","Antagonist"]
+	},
+	{
+		name: "Queen Chrysalis",
+		image: "chrysalis.png",
+		info: "Queen Chrysalis is the ruler of the changelings, creatures which can transform into other ponies and feed off of love and affection.",
+		categories: ["Character","Non-pony","Antagonist"]
+	},
+	{
+		name: "King Sombra",
+		image: "sombra.png",
+		info: "King Sombra is an evil tyrant who formerly ruled over the Crystal Empire and enslaved all the ponies there.",
+		categories: ["Character","Unicorn","Antagonist"]
+	},
+	{
+		name: "Lord Tirek",
+		image: "tirek.png",
+		info: "Lord Tirek is a centaur from a distant land. He was locked away in Tartarus for trying to steal all of the ponies' magic for his evil purposes.",
+		categories: ["Character","Non-pony","Antagonist"]
+	},
+	{
+		name: "Nightmare Moon",
+		image: "nightmare.png",
+		info: "Nightmare Moon is actually a form of Princess Luna. She came into being when Princess Luna grew very angry with her older sister, Princess Celestia.",
+		categories: ["Character","Alicorn","Antagonist"]
+	},
+	{
+		name: "The Flim Flam Brothers",
+		image: "flimflam.png",
+		info: "The Flim Flam Brothers are a pair of business ponies who always out to make a profit, usually at the cost of somepony else.",
+		categories: ["Character","Unicorn","Antagonist"]
+	},
+	{
+		name: "The Everfree Forest",
+		image: "everfree.png",
+		info: "The Everfree Forest lies just outside of Ponyville. It is a wild place, full of all kinds of dangerous plants and animals alike.",
+		categories: ["Location"]
+	},
+	{
+		name: "Canterlot",
+		image: "canterlot.png",
+		info: "Canterlot is the capital of Equestria and home of the unicorns. It is where Princess Celestia and Luna live, as well as Twilight Sparkle's family.",
+		categories: ["Location"]
+	}
+	,
+	{
+		name: "Ponyville",
+		image: "ponyville.png",
+		info: "Ponyville is the home of many of the characters from the show. It is unique in that Ponyville is home to Earth, Pegasus, and Unicorn ponies.",
+		categories: ["Location"]
+	},
+	{
+		name: "Cloudsdale",
+		image: "cloudsdale.png",
+		info: "Cloudsdale is the home of the pegasi. It flies all around Equestria in the sky, and it's also where all of the weather is made.",
+		categories: ["Location"]
+	},
+	{
+		name: "Appleoosa",
+		image: "appleoosa.png",
+		info: "Appleoosa is a town in the wild west. It is the home of settlers which grow apples in the orchards there.",
+		categories: ["Location"]
+	},
+	{
+		name: "The Crystal Empire",
+		image: "empire.png",
+		info: "The Crystal Empire is located in the cold north. For a thousand years the city disappeared, only to reappear one day.",
+		categories: ["Location"]
+	},
+	{
+		name: "Lyra Heartstrings",
+		image: "lyra.png",
+		info: "Lyra Heartstrings is a close friend of Bon Bon. The two of them are often seen sitting on park benches.",
+		categories: ["Character","Unicorn"]
+	},
+	{
+		name: "Bon Bon",
+		image: "bonbon.png",
+		info: "Bon Bon (aka Sweetie Drops) is a close friend of Lyra. She is often seen with her sitting on park benches. ",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Derpy Hooves",
+		image: "derpy.png",
+		info: "The one, the only, Derpy Hooves (aka Ditzy Doo / Muffins) is probably the fandom's favorite background pony. She is known to be a little klutzy at times, and her eyes are always crossed.",
+		categories: ["Character","Pegasus"]
+	},
+	{
+		name: "Vinyl Scratch",
+		image: "vinyl.png",
+		info: "Vinyl Scratch (aka DJ Pon3) is a musician. She's often seen disk jockeying parties with her two turn tables. ",
+		categories: ["Character","Unicorn"]
+	},
+	{
+		name: "Octavia",
+		image: "octavia.png",
+		info: "Octavia is a musician. She is often seen playing her cello at fancy parties and galas, especially ones in Canterlot.",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Dr. Hooves",
+		image: "dhooves.png",
+		info: "Dr. Hooves (aka Time Turner) is seen in the background of many episodes. Because of his hourglass cutie mark, some believe that he’s a time traveler. ",
+		categories: []
+	},
+	{
+		name: "Bulk Biceps",
+		image: "biceps.png",
+		info: "Bulk Biceps is a pegasus of nothing but muscle. Although it doesn't look like he could fly at all, he is often seen among the pegasi in episodes.",
+		categories: ["Character","Pegasus"]
+	},
+	{
+		name: "Gilda",
+		image: "gilda.png",
+		info: "Gilda is a griffon. She was once friends with Rainbow Dash, as the two of them met during flight camp when they were younger.",
+		categories: ["Character","Non-pony","Antagonist"]
+	},
+	{
+		name: "Trixie",
+		image: "trixie.png",
+		info: "The Great and Powerful Trixie need not a description for your eyes. Her actions speak for themselves.",
+		categories: ["Character","Unicorn","Antagonist"]
+	},
+	{
+		name: "Photo Finish",
+		image: "photo.png",
+		info: "Photo Finish is the paparazzo of the pony world. She's always taking tons of photos, trying to capture \"the magics.\"",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Sapphire Shores",
+		image: "sshores.png",
+		info: "Sapphire Shores is the pony of pop! She often has Rarity design outfits for her live performances.",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Moondancer",
+		image: "md1.png",
+		info: "Moondancer is a unicorn who studied magic in Canterlot. She attended Celestia's school for gifted unicorns alongside Twilight Sparkle, where they were friends.",
+		categories: ["Character","Unicorn"]
+	},
+	{
+		name: "Coloratura",
+		image: "coloratura.png",
+		info: "Coloratura (aka Rara) is the number one pop star in all of Equestria. She and Applejack were friends who attended summer camp together.",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Hoity Toity",
+		image: "ht.png",
+		info: "Hoity Toity is a fashion critic from Canterlot. As his name suggests, he's fairly obnoxious.",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Fancy Pants",
+		image: "fp.png",
+		info: "Fancy Pants is one of the Canterlot elite. He holds quite a bit of power and is one of the most important ponies in Canterlot.",
+		categories: ["Character","Unicorn"]
+	},
+	{
+		name: "Cheerilee",
+		image: "cheerilee.png",
+		info: "Cheerilee is the school teacher in Ponyville. She teaches all of the younger fillies and colts in the school house.",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Cranky Doodle",
+		image: "cranky.png",
+		info: "Cranky Doodle Donkey is a resident of Ponyville. While not the happiest character, he does love spending time with his wife, Matilda.",
+		categories: ["Character","Non-pony"]
+	},
+	{
+		name: "Babs Seed",
+		image: "babs.png",
+		info: "Babs Seed is Apple Bloom's cousin from Manehattan. She's also an out of town member of the Cutie Mark Crusaders.",
+		categories: ["Character","CMC","Earth Pony"]
+	},
+	{
+		name: "Lightning Dust",
+		image: "ld.png",
+		info: "Lightning Dust is a pegasus who's just as fast (if not faster) than Rainbow Dash. The two of them met in the Wonderbolts Academy.",
+		categories: ["Character","Pegasus"]
+	},
+	{
+		name: "Daring Do",
+		image: "daring.png",
+		info: "Daring Do is an adventurer featured in the novel series of the same name. She ventures around in the jungle fighting villains.",
+		categories: ["Character","Pegasus"]
+	},
+	{
+		name: "Coco Pommel",
+		image: "coco.png",
+		info: "Coco Pommel is a friend of Rarity. She's a dressmaker who works in Manehattan.",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Cheese Sandwich",
+		image: "cs.png",
+		info: "Cheese Sandwich is the ultimate party pony! He roams all around Equestria, going town to town, planning and throwing huge celebrations.",
+		categories: ["Character","Earth Pony"]
+	},
+	{
+		name: "Trenderhoof",
+		image: "th.png",
+		info: "Trenderhoof is a writer. He travels all around Equestria and writes about the culture he discovers.",
+		categories: ["Character","Unicorn"]
+	},
+	{
+		name: "Spitfire",
+		image: "sf.png",
+		info: "Spitfire is the captain of the Wonderbolts and is responsible for many of the decisions made by the group.",
+		categories: ["Character","Pegasus"]
+	},
+	{
+		name: "Soarin",
+		image: "soarin.png",
+		info: "Soarin is a member of the Wonderbolts, though he is the weakest flyer of the main group.",
+		categories: ["Character","Pegasus"]
+	},
+	{
+		name: "Fleetfoot",
+		image: "fleetfoot.png",
+		info: "Fleetfoot is a member of the Wonderbolts.",
+		categories: ["Character","Pegasus"]
+	},
+	{
+		name: "Silver Shill",
+		image: "sshill.png",
+		info: "Silver Shill is a sales pony who worked with the Flim Flam brothers.",
+		categories: ["Character","Earth Pony"]
+	}
+];
+
+/*
+	{
+		name: "New Card",
+		image: "",
+		info: "Description",
+		categories: []
+	}
+*/
+
+/*
+To make:
+Mane 6 - Elements of Harmony
+Mane 6 Vectors
+Episode Cards
+EG
+
+Snips
+Snails
+Pip Squeak
+Feather Weight
+
+Background
+
+Pets
+Owlicious
+Wanona
+Angel Bunny
+Tank
+Opalescence
+Gummy
+
+Steven Magnet
+
+Secondary Characters
+Trenderhoof
+Maud Pie & Pie Family
+Spitfire
+Soarin
+Fleetfoot
+Silver Shill
+
+Flutterbat
+Pinkamena 
+*/
